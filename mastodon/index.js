5c37028221497b3c6b4c1a0f8e429cf09a06c5e5
Kotaro Kawashima <utageyanagi+kkgithub@gmail.com>
2017-04-15 16:30:53 +0900

    Add: Mastodon plugin

---
+++ mastodon/index.js
@@ -0,0 +1,20 @@
+// just pass through Franz
+module.exports = (Franz) => {
+  class Mastodon extends Franz{
+    validateServer(URL){
+      const api = `${URL}/manifest.json`;
+      return new Promise((resolve, reject) => {
+        $.get(api, (resp) => {
+          if (typeof(resp) === 'object' && 'name' in resp && resp.name === 'Mastodon') {
+            resolve();
+          }else{
+            reject();
+          }
+
+        }).fail(reject);
+      });
+    }
+  }
+
+  return Mastodon;
+};
